0bae11bff2127bcc0e15ab6064cc86eaad4c7ffc
Virendrabhalerao <147663052+Virendrabhalerao@users.noreply.github.com>
2023-10-12 02:06:48 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from pythonyugeapitest import get_product_info  # Replace 'your_script_name' with the actual name of your script file
+
+def main():
+    st.title("Product Information App")
+
+    api_key = "1BUjWKufjWMFTmCKz1474bmcbeRRnmicExN"
+    product_name = st.text_input("Enter the product name:")
+
+    if st.button("Get Product Info"):
+        product_info = get_product_info(api_key, product_name)
+
+        if product_info:
+            st.subheader("Product Info:")
+            st.write("Product Title:", product_info[0]['product_title'])
+            st.write("Lowest Price:", product_info[0]['product_lowest_price'])
+            st.write("Product Link:", product_info[0]['product_link'])
+            st.image(product_info[0]['product_image'], caption="Product Image", use_column_width=True)
+        else:
+            st.warning("No product found with the given name.")
+
+if __name__ == "__main__":
+    main()
---
+++ pythonyugeapitest.py
@@ -0,0 +1,36 @@
+import requests
+
+def get_product_info(api_key, product_name):
+    url = "https://price-api.datayuge.com/api/v1/compare/search"
+
+    querystring = {
+        "api_key": api_key,
+        "product": product_name,
+        "page": "1"
+    }
+
+    headers = {'content-type': 'application/json'}
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    data = response.json()
+
+    if 'data' in data:
+        product_info = data['data']
+        return product_info
+    else:
+        return None
+
+if __name__ == "__main__":
+    api_key = "1BUjWKufjWMFTmCKz1474bmcbeRRnmicExN"
+    product_name = input("Enter the product name: ")
+
+    product_info = get_product_info(api_key, product_name)
+
+    if product_info:
+        print("Product Info:")
+        print("Product Title:", product_info[0]['product_title'])
+        print("Lowest Price:", product_info[0]['product_lowest_price'])
+        print("Product Link:", product_info[0]['product_link'])
+        print("Product Image:", product_info[0]['product_image'])
+    else:
+        print("No product found with the given name.")
